5a16934fe0324e19c62ea742ad1d98ca6a3ebb49
Jeon Minji <mjjeon2645@gmail.com>
2022-07-04 11:51:39 +0900

    20220704-Calculator-전민지
    이월 3주차 Calculator 강의 인출학습

---
+++ week03/Calculator/20220704-전민지/app/src/main/java/CoreCalculator.java
@@ -0,0 +1,35 @@
+public class CoreCalculator {
+  public static final String[] OPERATORS = new String[]{"+", "-", "*", "/", "="};
+
+  private long currentNumber = 0;
+  private long accumulator = 0;
+  private String currentOperator = "";
+
+  public long getCurrentNumber() {
+    return currentNumber;
+  }
+
+  public long getAccumulator() {
+    return accumulator;
+  }
+
+  public void addNumber(int number) {
+    currentNumber *= 10;
+    currentNumber += number;
+  }
+
+  public void updateOperator(String operator) {
+    currentOperator = operator;
+  }
+
+  public void calculate() {
+    switch (currentOperator) {
+      case "+" -> accumulator += currentNumber;
+      case "-" -> accumulator -= currentNumber;
+      case "*" -> accumulator *= currentNumber;
+      case "/" -> accumulator /= currentNumber;
+      case "" -> accumulator = currentNumber;
+    }
+    currentNumber = 0;
+  }
+}
