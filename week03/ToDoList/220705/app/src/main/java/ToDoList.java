f9cbf225cfa67729cda1213398bd93ad114c9641
myunghoonkim <xcv8826@gmail.com>
2022-07-05 23:57:25 +0900

    투두리스트 만들기 (리스트를 사용하지 않고 그냥 패널 요소로 삭제와 업로드를 함)

---
+++ week03/ToDoList/220705/app/src/main/java/ToDoList.java
@@ -0,0 +1,105 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.util.List;
+
+public class ToDoList {
+  private List<String> numberOfButtons = new ArrayList<>();;
+
+  public static void main(String[] args) {
+      ToDoList application = new ToDoList();
+      application.run();
+  }
+
+  public void run() {
+
+    JFrame frame = new JFrame("Todo List");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(400,600);
+
+    frame.getContentPane().setBackground(Color.lightGray);
+
+    JPanel mainPanel = new JPanel();
+    mainPanel.setLayout(new BorderLayout());
+    frame.add(mainPanel);
+
+    //메인 제목 레이블
+    JLabel label = new JLabel("할 일 목록");
+    frame.add(label,BorderLayout.PAGE_START);
+    label.setHorizontalAlignment(JLabel.CENTER);
+
+
+
+    //메뉴 패널
+    JPanel menuPanel = new JPanel();
+    mainPanel.add(menuPanel,BorderLayout.NORTH);
+
+    JLabel label1 = new JLabel("할 일");
+    menuPanel.add(label1);
+
+    JTextField textField = new JTextField(10);
+    menuPanel.add(textField);
+
+    JButton button = new JButton("추가");
+    menuPanel.add(button);
+
+    JPanel toDoPanel = new JPanel();
+    mainPanel.add(toDoPanel,BorderLayout.CENTER);
+
+    JPanel listPanelOntoDoPanel = new JPanel();
+    listPanelOntoDoPanel.setLayout(new GridLayout(numberOfButtons.size(),1));
+    toDoPanel.add(listPanelOntoDoPanel);
+
+
+
+
+    button.addActionListener(event -> {
+
+      JPanel pairPanel = new JPanel();
+      pairPanel.setLayout(new GridLayout(1,2));
+
+      JCheckBox toDoCheckBox = new JCheckBox(textField.getText());
+      pairPanel.add(toDoCheckBox);
+
+      JButton xButton = new JButton(" X ");
+      xButton.addActionListener(xEvent -> {
+        listPanelOntoDoPanel.remove(pairPanel);
+        listPanelOntoDoPanel.setVisible(false);
+        listPanelOntoDoPanel.setVisible(true);
+
+        numberOfButtons.add("button");
+      });
+
+      pairPanel.add(xButton);
+
+      listPanelOntoDoPanel.add(pairPanel);
+
+      toDoCheckBox.addMouseListener(new MouseAdapter() {
+        public void mouseClicked(MouseEvent event) {
+          if (toDoCheckBox.isSelected()) {
+            toDoCheckBox.setSelected(false);
+          }
+          if (!toDoCheckBox.isSelected()) {
+            toDoCheckBox.setSelected(true);
+          }
+        }
+    });
+
+
+
+    frame.setVisible(true);
+
+  });
+
+
+
+
+
+
+
+
+    frame.setVisible(true);
+
+  }
+}
