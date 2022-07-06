202aa3c1b1be6bb9eb8391901658792fdeaa890a
Jeon Minji <mjjeon2645@gmail.com>
2022-07-06 10:56:53 +0900

    작성한 text에 의거하여 1개의 태스크 추가 가능
    1. 태스크 추가 가능
    2. 각 버튼 별 actionListener 추가 필요
    3. 하단으로 수직 나열 레이아웃 정립 필요

--- week03/todolist/20220706-전민지2/app/src/main/java/TodoList.java
+++ week03/todolist/20220706-전민지2/app/src/main/java/TodoList.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TodoList {
+  private JPanel taskListPanel;
+
   public static void main(String[] args) {
     TodoList application = new TodoList();
     application.run();
@@ -26,21 +28,48 @@ public class TodoList {
     contentPanel.setBackground(Color.yellow);
     frame.add(contentPanel);
 
+    // taskList 패널 실현
+    taskListPanel = new JPanel();
+    taskListPanel.setBackground(Color.DARK_GRAY);
+    contentPanel.add(taskListPanel);
+
     // 상단 form(라벨, 텍스트필드, 추가버튼) 세팅
-    JPanel formPanel = new JPanel();
-    formPanel.setBackground(Color.pink);
-    contentPanel.add(formPanel, BorderLayout.PAGE_START);
+    JPanel taskAddFormPanel = new JPanel();
+    taskAddFormPanel.setBackground(Color.pink);
+    contentPanel.add(taskAddFormPanel, BorderLayout.PAGE_START);
 
     JLabel subtitleLabel = new JLabel("할 일: ");
-    formPanel.add(subtitleLabel);
+    taskAddFormPanel.add(subtitleLabel);
 
     JTextField textField = new JTextField(10);
-    formPanel.add(textField);
+    taskAddFormPanel.add(textField);
 
     JButton addButton = new JButton("추가");
     addButton.addActionListener(event -> {
+      String text = textField.getText();
 
+      // 1개의 Task에 대한 컴포넌트를 묶어줄 패널 세팅 및 컴포넌트 추가
+      JPanel taskFormPanel = new JPanel();
+      taskFormPanel.setBackground(Color.GREEN);
+      taskListPanel.add(taskFormPanel);
+
+      JCheckBox checkBox = new JCheckBox();
+      taskFormPanel.add(checkBox);
+
+      JLabel taskTitleLabel = new JLabel(text);
+      taskFormPanel.add(taskTitleLabel);
+
+      JButton deleteTaskButton = new JButton("X");
+      taskFormPanel.add(deleteTaskButton);
+
+
+      contentPanel.setVisible(false);
+      contentPanel.setVisible(true);
+      frame.setVisible(true);
     });
-    formPanel.add(addButton);
+    taskAddFormPanel.add(addButton);
+
+    // 하단 list 패널 세팅? 과연 필요할까?
+
   }
 }
