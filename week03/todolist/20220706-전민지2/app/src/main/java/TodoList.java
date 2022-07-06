d37b8e401ed8a2ae03573eab0ed8f52883b1aa68
Jeon Minji <mjjeon2645@gmail.com>
2022-07-06 16:24:55 +0900

    todo list 구현(패널 및 프레임 백그라운드 컬러 삭제 전)

--- week03/todolist/20220706-전민지2/app/src/main/java/TodoList.java
+++ week03/todolist/20220706-전민지2/app/src/main/java/TodoList.java
@@ -1,8 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class TodoList {
-  private JPanel taskListPanel;
+  private JFrame frame;
+  private JPanel contentPanel;
+  private JPanel displayTasksPanel;
+  private JPanel titleAndFormContainerPanel;
+  private JPanel formPanel;
+  private JTextField textField;
 
   public static void main(String[] args) {
     TodoList application = new TodoList();
@@ -11,65 +18,126 @@ public class TodoList {
 
   public void run() {
     // frame 세팅
-    JFrame frame = new JFrame("Todo List");
+    initFrame();
+
+    // 컨텐트 패널 세팅
+    initContentPanel();
+
+    // 상단 타이틀 및 폼
+    initTitleAndForm();
+
+    // task 목록들이 노출되는 패널 실현
+    initDisplayTasksPanel();
+  }
+
+  public void initTitleAndForm() {
+    // 타이틀과 폼을 묶어주는 컨테이너 패널 생성
+    initTitleAndFormContainer();
+
+    // 타이틀 생성 및 세팅
+    initTitle();
+
+    // form(소제목, 텍스트필드, 할일 추가버튼) 세팅
+    initFormComponentsContainer();
+
+    initSubtitleInForm();
+
+    initTextFieldInForm();
+
+    JButton addButton = new JButton("추가");
+    addButton.addActionListener(addEvent -> {
+      String text = textField.getText();
+
+      // 1개의 Task에 대한 컴포넌트를 묶어줄 패널 세팅 및 컴포넌트 추가
+      JPanel taskComponentContainerPanel = new JPanel();
+      taskComponentContainerPanel.setBackground(Color.GREEN);
+      displayTasksPanel.add(taskComponentContainerPanel);
+
+      JCheckBox checkBox = new JCheckBox();
+      taskComponentContainerPanel.add(checkBox);
+
+      JLabel taskTitleLabel = new JLabel(text);
+      taskTitleLabel.addMouseListener(new MouseAdapter() {
+        public void mouseClicked(MouseEvent e) {
+          if (checkBox.isSelected()) {
+            checkBox.setSelected(false);
+            return;
+          }
+            checkBox.setSelected(true);
+          }
+      });
+
+      taskComponentContainerPanel.add(taskTitleLabel);
+
+      initDeleteTaskButton(taskComponentContainerPanel);
+
+      showContentPanel();
+    });
+    formPanel.add(addButton);
+  }
+
+  public void initFrame() {
+    frame = new JFrame("Todo List");
     frame.setBackground(Color.cyan);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(400, 600);
     frame.setVisible(true);
+  }
 
-    // 타이틀 라벨 세팅
+  public void initTitleAndFormContainer() {
+    titleAndFormContainerPanel = new JPanel();
+    titleAndFormContainerPanel.setLayout(new BorderLayout());
+    frame.add(titleAndFormContainerPanel, BorderLayout.PAGE_START);
+  }
+
+  public void initTitle() {
     JLabel titleLabel = new JLabel("할 일 목록");
     titleLabel.setHorizontalAlignment(JTextField.CENTER);
-    frame.add(titleLabel, BorderLayout.PAGE_START);
+    titleAndFormContainerPanel.add(titleLabel, BorderLayout.PAGE_START);
+  }
 
-    // 컨텐트 패널 세팅
-    JPanel contentPanel = new JPanel();
-    contentPanel.setLayout(new BorderLayout());
+  public void initFormComponentsContainer() {
+    formPanel = new JPanel();
+    formPanel.setBackground(Color.pink);
+    titleAndFormContainerPanel.add(formPanel);
+  }
+
+  public void initSubtitleInForm() {
+    JLabel subtitleLabel = new JLabel("할 일: ");
+    formPanel.add(subtitleLabel);
+  }
+
+  public void initTextFieldInForm() {
+    textField = new JTextField(10);
+    formPanel.add(textField);
+  }
+
+  public void initContentPanel() {
+    contentPanel = new JPanel();
     contentPanel.setBackground(Color.yellow);
     frame.add(contentPanel);
+  }
 
-    // taskList 패널 실현
-    taskListPanel = new JPanel();
-    taskListPanel.setBackground(Color.DARK_GRAY);
-    contentPanel.add(taskListPanel);
-
-    // 상단 form(라벨, 텍스트필드, 추가버튼) 세팅
-    JPanel taskAddFormPanel = new JPanel();
-    taskAddFormPanel.setBackground(Color.pink);
-    contentPanel.add(taskAddFormPanel, BorderLayout.PAGE_START);
-
-    JLabel subtitleLabel = new JLabel("할 일: ");
-    taskAddFormPanel.add(subtitleLabel);
-
-    JTextField textField = new JTextField(10);
-    taskAddFormPanel.add(textField);
-
-    JButton addButton = new JButton("추가");
-    addButton.addActionListener(event -> {
-      String text = textField.getText();
-
-      // 1개의 Task에 대한 컴포넌트를 묶어줄 패널 세팅 및 컴포넌트 추가
-      JPanel taskFormPanel = new JPanel();
-      taskFormPanel.setBackground(Color.GREEN);
-      taskListPanel.add(taskFormPanel);
-
-      JCheckBox checkBox = new JCheckBox();
-      taskFormPanel.add(checkBox);
-
-      JLabel taskTitleLabel = new JLabel(text);
-      taskFormPanel.add(taskTitleLabel);
-
-      JButton deleteTaskButton = new JButton("X");
-      taskFormPanel.add(deleteTaskButton);
-
-
-      contentPanel.setVisible(false);
-      contentPanel.setVisible(true);
-      frame.setVisible(true);
+  public void initDeleteTaskButton(JPanel taskComponentContainerPanel) {
+    JButton deleteTaskButton = new JButton("X");
+    deleteTaskButton.addActionListener(deleteEvent -> {
+      displayTasksPanel.remove(taskComponentContainerPanel);
+      displayTasksPanel.setVisible(false);
+      displayTasksPanel.setVisible(true);
     });
-    taskAddFormPanel.add(addButton);
+    taskComponentContainerPanel.add(deleteTaskButton);
+  }
 
-    // 하단 list 패널 세팅? 과연 필요할까?
+  public void showContentPanel() {
+    contentPanel.setVisible(false);
+    contentPanel.setVisible(true);
+    frame.setVisible(true);
+  }
 
+  public void initDisplayTasksPanel() {
+    displayTasksPanel = new JPanel();
+    displayTasksPanel.setBackground(Color.DARK_GRAY);
+    displayTasksPanel.setLayout(new GridLayout(0, 1));
+    contentPanel.add(displayTasksPanel);
   }
 }
