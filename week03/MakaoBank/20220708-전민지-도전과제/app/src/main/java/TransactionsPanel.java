129bf32afc612d91b5fe8c455ac5521bd09fb3ad
Jeon Minji <mjjeon2645@gmail.com>
2022-07-08 15:17:28 +0900

    거래 내역 패널 레이아웃 구현
    거래 내역 패널 레이아웃 구현

--- week03/MakaoBank/20220708-전민지-도전과제/app/src/main/java/TransactionsPanel.java
+++ week03/MakaoBank/20220708-전민지-도전과제/app/src/main/java/TransactionsPanel.java
@@ -1,8 +1,25 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class TransactionsPanel extends JPanel {
   public TransactionsPanel(Account account) {
 
+    this.setLayout(new GridLayout(account.transactionsOfMine().size() + 1, 2));
+
+    // TODO. 거래내역이 보여야 함(내 계좌에 한한 내용만 보여도 됨 우선은)
+
+    JLabel titleLabel = new JLabel("거래 내역");
+    this.add(titleLabel);
+
+    JLabel nullLabel = new JLabel("    ");
+    this.add(nullLabel);
+
+    for (String transaction : account.transactionsOfMine()) {
+      JLabel identifierOfOthersLabel = new JLabel("계좌번호: " + account.identifierOfOthers());
+      this.add(identifierOfOthersLabel);
+
+      JLabel transactionLabel = new JLabel(transaction);
+      this.add(transactionLabel);
+    }
   }
-  // TODO. 거래내역이 보여야 함(내 계좌에 한한 내용만 보여도 됨 우선은)
 }
