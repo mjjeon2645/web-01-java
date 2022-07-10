505e4d6a3efcb2195d931a84c6b560abb72d9d5a
Jeon Minji <mjjeon2645@gmail.com>
2022-07-11 08:06:09 +0900

    20220711-BuildSystem-JUnit-전민지
    강의인출학습

---
+++ week04/BuildSystemJUnit/20220711-전민지/demo/src/test/java/HelloWorldTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldTest {
+  @Test
+  void defaultMessage() {
+    MessageProvider messageProvider = new MessageProvider();
+
+    assertEquals("Hello, world!", messageProvider.message());
+  }
+
+  @Test
+  void HelloToYou() {
+    MessageProvider messageProvider = new MessageProvider("Ashal");
+
+    assertEquals("Hello, Ashal!", messageProvider.message());
+  }
+}
