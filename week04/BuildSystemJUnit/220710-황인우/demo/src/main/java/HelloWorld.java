a8d279c55fb97133b58f489438967b7f1f968cde
Inwoo Hwang <hsjkdss228@naver.com>
2022-07-11 10:49:03 +0900

    기능 구현 완료

---
+++ week04/BuildSystemJUnit/220710-황인우/demo/src/main/java/HelloWorld.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    HelloWorld helloWorld = new HelloWorld();
+    helloWorld.run();
+  }
+
+  public void run() {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("What's your name?");
+
+    String name = scanner.nextLine();
+
+    MessageProvider messageProvider = new MessageProvider(name);
+
+    System.out.println(messageProvider.message());
+  }
+}
