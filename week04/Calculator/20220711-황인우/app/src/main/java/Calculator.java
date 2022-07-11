4b993a220b76681bbae0c254c18ff2251022dc97
Inwoo Hwang <hsjkdss228@naver.com>
2022-07-11 11:29:38 +0900

    사칙연산 수행 후 결과를 검증하는 테스트 코드 작성 및 검증

--- week04/Calculator/20220711-황인우/app/src/main/java/Calculator.java
+++ week04/Calculator/20220711-황인우/app/src/main/java/Calculator.java
@@ -28,11 +28,13 @@ public class Calculator {
     int number1 = Integer.parseInt(components[0]);
     int number2 = Integer.parseInt(components[2]);
 
-    if (components[1].equals("+")) {
-      return number1 + number2;
-    }
-
-    return number1 - number2;
+    return switch (components[1]) {
+      case "+" -> number1 + number2;
+      case "-" -> number1 - number2;
+      case "*" -> number1 * number2;
+      case "/" -> number1 / number2;
+      default -> 0;
+    };
   }
 
   public void print(int result) {
--- week04/Calculator/20220711-황인우/app/src/test/java/CalculatorTest.java
+++ week04/Calculator/20220711-황인우/app/src/test/java/CalculatorTest.java
@@ -29,4 +29,22 @@ class CalculatorTest {
     assertEquals(0, calculator.process("5 - 5"));
     assertEquals(3, calculator.process("8 - 5"));
   }
+
+  @Test
+  void times() {
+    Calculator calculator = new Calculator();
+
+    assertEquals(0, calculator.process("0 * 3"));
+    assertEquals(25, calculator.process("5 * 5"));
+    assertEquals(40, calculator.process("8 * 5"));
+  }
+
+  @Test
+  void divides() {
+    Calculator calculator = new Calculator();
+
+    assertEquals(1, calculator.process("12452343 / 12452343"));
+    assertEquals(2, calculator.process("24 / 12"));
+    assertEquals(10, calculator.process("100 / 10"));
+  }
 }
