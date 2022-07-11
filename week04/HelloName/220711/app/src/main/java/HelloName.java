7f853b8f506e374b4fdd7a3f9cdfcf96a9be592a
myunghoonkim <xcv8826@gmail.com>
2022-07-11 11:33:14 +0900

    20220711-HelloWorld-kmh
    hella~

---
+++ week04/HelloName/220711/app/src/main/java/HelloName.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class HelloName {
+  public static void main(String[] args) {
+    HelloName application = new HelloName();
+    application.run();
+  }
+
+  public void run() {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("[HelloWorld]");
+
+    System.out.println("What's your name?");
+
+    String name = scanner.nextLine();
+
+    System.out.println("Hello, " + name + "!");
+
+  }
+}
