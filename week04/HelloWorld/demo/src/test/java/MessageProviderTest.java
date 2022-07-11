37a6788d36ea5c14da09932fb0e0a1729d35f83f
Jeon Minji <mjjeon2645@gmail.com>
2022-07-11 13:29:57 +0900

    20220711-HelloWorld-황인우-조성환-전민지
    짝프로그래밍(HelloWorld)

---
+++ week04/HelloWorld/demo/src/test/java/MessageProviderTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageProviderTest {
+  @Test
+  void defaultMessage() {
+    MessageProvider messageProvider = new MessageProvider();
+
+    assertEquals("Hello, world!", messageProvider.message());
+  }
+
+  @Test
+  void messageToYou() {
+    MessageProvider messageProvider = new MessageProvider("Chikorita");
+
+    assertEquals("Hello, Chikorita!", messageProvider.message());
+  }
+}
