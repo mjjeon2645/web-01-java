82ddc09bd81590e0e9e5aa86666fb35f9cc87f4f
seungjjun <il1uso@naver.com>
2022-07-12 14:54:59 +0900

    20220712-makaoBank-노승준

---
+++ week04/MakaoBank/20220712-노승준/MakaoBank/src/test/java/models/AccountTest.java
@@ -0,0 +1,49 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+
+import javax.annotation.processing.AbstractProcessor;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+  @Test
+  void processWithoutTransaction() {
+    Account account = new Account();
+
+    assertEquals(List.of(), account.process(List.of()));
+  }
+
+  @Test
+  void processOneTransaction() {
+    Transaction transaction = new Transaction("잔액", 1000);
+    TransactionResult transactionResult = new TransactionResult(transaction, 1000);
+
+    Account account = new Account();
+
+    List<Transaction> transactions = List.of(transaction);
+
+    assertEquals(List.of(transactionResult), account.process(transactions));
+  }
+
+  @Test
+  void processTransactions() {
+    List<Transaction> transactions = List.of(
+        new Transaction("잔액", 1000),
+        new Transaction("입금", 2000),
+        new Transaction("출금", 100)
+    );
+
+    List<TransactionResult> transactionResults = List.of(
+        new TransactionResult(transactions.get(0), 1000),
+        new TransactionResult(transactions.get(1), 3000),
+        new TransactionResult(transactions.get(2), 2900)
+    );
+
+    Account account = new Account();
+
+    assertEquals(transactionResults, account.process(transactions));
+  }
+}
---
+++ week04/MakaoBank/20220712-노승준/MakaoBank/src/test/java/models/TransactionResultTest.java
@@ -0,0 +1,28 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionResultTest {
+  @Test
+  void creation() {
+    Transaction transaction = new Transaction("입금", 2000);
+    int amount = 3000;
+    TransactionResult transactionResult =
+        new TransactionResult(transaction, amount);
+
+    assertArrayEquals(new String[]{"입금","2000","3000"},
+        transactionResult.values());
+  }
+
+  @Test
+  void toCsvRow() {
+    Transaction transaction = new Transaction("입금", 2000);
+    int amount = 3000;
+    TransactionResult transactionResult =
+        new TransactionResult(transaction, amount);
+
+    assertEquals("입금,2000,3000", transactionResult.toCsvRow());
+  }
+}
