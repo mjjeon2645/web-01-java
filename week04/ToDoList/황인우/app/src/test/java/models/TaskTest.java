948abb01bf19ed17bad65e44a8cf7d20906fbf96
Inwoo Hwang <hsjkdss228@naver.com>
2022-07-14 02:11:08 +0900

    Task, TaskRepository 클래스에 대한 단위 테스트 코드 작성 및 검증

--- week04/ToDoList/황인우/app/src/test/java/models/TaskTest.java
+++ week04/ToDoList/황인우/app/src/test/java/models/TaskTest.java
@@ -7,8 +7,39 @@ import static org.junit.jupiter.api.Assertions.*;
 class TaskTest {
   @Test
   void creation() {
-    Task task = new Task("할일 1");
+    Task task = new Task("레이시티 같은 오픈 월드 레이싱 게임의" +
+        " 게임플레이 월드를 구성하는 요소들에 뭐가 있을지 파악하기");
   }
 
+  @Test
+  void text() {
+    Task task = new Task("포켓몬스터 게임 디렉터 되기");
 
+    assertEquals("포켓몬스터 게임 디렉터 되기", task.text());
+  }
+
+  @Test
+  void setStatusDone() {
+    Task task = new Task("자바 척척석사 되기");
+    task.setStatusDone();
+
+    assertEquals("DONE", task.status());
+  }
+
+  @Test
+  void setStatusToDo() {
+    Task task = new Task("토익 800점 넘겨서 졸업요건 맞추기");
+    task.setStatusDone();
+    task.setStatusToDo();
+
+    assertEquals("TO_DO", task.status());
+  }
+
+  @Test
+  void setStatusDeleted() {
+    Task task = new Task("반나절 쉬면서 심신 다잡기");
+    task.setStatusDeleted();
+
+    assertEquals("DELETED", task.status());
+  }
 }
--- week04/ToDoList/황인우/app/src/test/java/repositories/TaskRepositoryTest.java
+++ week04/ToDoList/황인우/app/src/test/java/repositories/TaskRepositoryTest.java
@@ -3,6 +3,25 @@ package repositories;
 import models.Task;
 import org.junit.jupiter.api.Test;
 
-class TaskRepositoryTest {
+import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TaskRepositoryTest {
+  @Test
+  void getTasks() {
+    TaskRepository taskRepository = new TaskRepository(new ArrayList<>());
+
+    assertEquals(0, taskRepository.getTasks().size());
+  }
+
+  @Test
+  void addTask() {
+    TaskRepository taskRepository = new TaskRepository(new ArrayList<>());
+
+    taskRepository.addTask(new Task("나는 레드타입 휴먼 헌터 스페셜리스트 I.NU"));
+    taskRepository.addTask(new Task("눈에 보이는 것들은 뭐든지 코.딩.한.다"));
+
+    assertEquals(2, taskRepository.getTasks().size());
+  }
 }
