9ca4879c8004e93ce287b302284048c8ac1703ae
myunghoonkim <xcv8826@gmail.com>
2022-07-13 10:57:54 +0900

    20220713-PickyCalculator-kmh
    숫자 하나일때 프로그램 꺼지는 오류 수정 완료
    
    (continue를 해주지 않아 인식후 다음 조건식으로 들어가버린후 꺼짐)

--- week04/PickyCalculator/220712/app/src/test/java/PickyCalculatorTest.java
+++ week04/PickyCalculator/220712/app/src/test/java/PickyCalculatorTest.java
@@ -1,4 +1,60 @@
+import org.junit.jupiter.api.*;
+
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PickyCalculatorTest {
-  
+  @Test
+  void coreCalculate() {
+    PickyCalculator pickyCalculator = new PickyCalculator();
+
+    assertEquals(4, pickyCalculator.compute("1 + 3"));
+    assertEquals(6, pickyCalculator.compute("3 * 2"));
+    assertEquals(2, pickyCalculator.compute("4 / 2"));
+    assertEquals(3, pickyCalculator.compute("5 - 2"));
+  }
+
+  @Test
+  void testExpressionError() {
+    PickyCalculator pickyCalculator = new PickyCalculator();
+
+    assertTrue(pickyCalculator.testExpressionError("1 + 1"));
+    assertFalse(pickyCalculator.testExpressionError("1 +"));
+    assertFalse(pickyCalculator.testExpressionError("1+1"));
+    assertFalse(pickyCalculator.testExpressionError("+ 1 1"));
+    assertFalse(pickyCalculator.testExpressionError("1 1 1"));
+    assertFalse(pickyCalculator.testExpressionError("*"));
+    assertFalse(pickyCalculator.testExpressionError("12+"));
+    assertFalse(pickyCalculator.testExpressionError("123 + 2 *"));
+    assertFalse(pickyCalculator.testExpressionError("123 * $"));
+    assertTrue(pickyCalculator.testExpressionError("123"));
+    assertTrue(pickyCalculator.testExpressionError("4"));
+
+  }
+
+  @Test
+  void isOneNumber() {
+    PickyCalculator pickyCalculator = new PickyCalculator();
+
+    assertTrue(pickyCalculator.isOneNumber("3 "));
+    assertFalse(pickyCalculator.isOneNumber("3 +"));
+  }
+
+  @Test
+  void oneNumberPrintOut() {
+    PickyCalculator pickyCalculator = new PickyCalculator();
+    String[] components = new String[]{"1"};
+    if (components.length == 1) {
+      System.out.println("Result: " + components[0]);
+
+    }
+  }
+
+  @Test
+  void testAll() {
+    PickyCalculator pickyCalculator = new PickyCalculator();
+    Scanner scanner = new Scanner(System.in);
+
+  }
 }
