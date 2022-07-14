dd2a60b2125a3f0ee73acdbfb362e2d517619e88
Inwoo Hwang <hsjkdss228@naver.com>
2022-07-14 11:09:54 +0900

    주간 과제 1 HelloName 기능 구현 및 검증 완료

---
+++ week04/HelloName/20220714-황인우/app/src/main/java/MessageProvider.java
@@ -0,0 +1,16 @@
+public class MessageProvider {
+  private final String name;
+
+  public MessageProvider() {
+    this.name = "world";
+  }
+
+  public MessageProvider(String name) {
+    this.name = name;
+  }
+
+
+  public String message() {
+    return "Hello, " + name + "!";
+  }
+}
