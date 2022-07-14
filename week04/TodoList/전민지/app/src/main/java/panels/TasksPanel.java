a7fe96789668326b40d0a6ff09685d0af6e0e37e
Jeon Minji <mjjeon2645@gmail.com>
2022-07-14 16:25:15 +0900

    test용 Commit(branch 작동상황 체크중)

--- week04/TodoList/전민지/app/src/main/java/panels/TasksPanel.java
+++ week04/TodoList/전민지/app/src/main/java/panels/TasksPanel.java
@@ -15,7 +15,7 @@ public class TasksPanel extends JPanel {
   public TasksPanel(Tasks tasks, TodoList todoList) {
     this.todoListFrame = todoList;
     this.tasks = tasks;
-    this.setLayout(new GridLayout(0, 1));
+    this.setLayout(new GridLayout(5, 1));
 
     for (Task task : tasks.getTasks()) {
       initTask(task);
