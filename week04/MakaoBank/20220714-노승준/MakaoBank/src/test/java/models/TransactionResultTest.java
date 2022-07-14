03188b74df3658c21081fd67e1ff185c0fe6df18
seungjjun <il1uso@naver.com>
2022-07-14 12:19:46 +0900

    4주차 목요일 주간과제 "MakaoBank"

---
+++ week04/MakaoBank/20220714-노승준/MakaoBank/src/test/java/models/TransactionResultTest.java
@@ -0,0 +1,31 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionResultTest {
+  @Test
+  void creation() {
+    Transaction transaction = new Transaction("입금", 2000);
+    int amount = 1000 + 2000;
+
+    TransactionResult transactionResult =
+        new TransactionResult(transaction, amount);
+
+    assertArrayEquals(new String[]{"입금", "2000", "3000"},
+        transactionResult.values());
+  }
+
+  @Test
+  void toCsvRow() {
+    Transaction transaction = new Transaction("입금", 2000);
+    int amount = 1000 + 2000;
+
+    TransactionResult transactionResult =
+        new TransactionResult(transaction, amount);
+
+    assertEquals("입금,2000,3000", transactionResult.toCsvRow());
+
+  }
+}
