71a7183787f6470a93212ec274538d638b30b6e6
sunghwan22 <tidls3144@naver.com>
2022-07-14 11:30:14 +0900

    20220714-반복과제-조성환

--- week04/JUnit/20220714-조성환/app/src/main/java/HelloName.java
+++ week04/JUnit/20220714-조성환/app/src/main/java/HelloName.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class HelloName {
+import java.util.Scanner;
+
+public class HelloName {
+  private MessageProvider messageProvider;
+
+  public static void main(String[] args) {
+    HelloName application = new HelloName();
+    application.run();
+  }
+
+  private void run() {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("[HelloWorld]");
+    System.out.println("what'your name?");
+
+    String name = scanner.nextLine();
+
+    messageProvider = new MessageProvider(name);
+
+    System.out.println(messageProvider.message());
+  }
 }
--- week04/JUnit/20220714-조성환/app/src/main/java/MessageProvider.java
+++ week04/JUnit/20220714-조성환/app/src/main/java/MessageProvider.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class MessageProvider {
+public class MessageProvider {
+  private final String name;
+
+  public MessageProvider() {
+    this.name = "World";
+  }
+
+  public MessageProvider(String name) {
+    this.name = name;
+  }
+
+  public String message() {
+    return "Hello, " + name + "!";
+  }
 }
