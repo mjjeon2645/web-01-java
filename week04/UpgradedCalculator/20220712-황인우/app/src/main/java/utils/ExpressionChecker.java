503f3501034ab8b8ed76a8135b29dc8b24d4cc87
Inwoo Hwang <hsjkdss228@naver.com>
2022-07-15 09:48:10 +0900

    수정 요구사항을 반영하여 소스코드 수정 (3)
    - ExpressionChecker 내 메서드들의 네이밍 수정
     with '#' will be ignored, and an empty message aborts the commit.

--- week04/UpgradedCalculator/20220712-황인우/app/src/main/java/utils/ExpressionChecker.java
+++ week04/UpgradedCalculator/20220712-황인우/app/src/main/java/utils/ExpressionChecker.java
@@ -1,23 +1,31 @@
 package utils;
 
 public class ExpressionChecker {
+  private static final String[] SUPPORTED_OPERATORS = {"+", "-", "*", "/"};
+
   public boolean checkValidity(String expression) {
     String[] words = expression.split(" ");
 
-    if (words.length == 1 && isLegalWord(words[0])) {
+    if (words.length == 1 && isNumber(words[0])) {
       return true;
     }
 
     return words.length == 3
-        && isLegalWord(words[0]) && isLegalWord(words[2])
-        && isLegalOperator(words[1]);
+        && isNumber(words[0]) && isNumber(words[2])
+        && isSupportedOperator(words[1]);
   }
 
-  private boolean isLegalOperator(String word) {
-    return word.matches("[+]|[-]|[*]|[/]");
+  private boolean isSupportedOperator(String word) {
+    for (String operator : SUPPORTED_OPERATORS) {
+      if (word.equals(operator)) {
+        return true;
+      }
+    }
+
+    return false;
   }
 
-  public boolean isLegalWord(String word) {
+  public boolean isNumber(String word) {
     for (int i = 0; i < word.length(); i += 1) {
       char letter = word.charAt(i);
 
