59ef5748651f35e34fcf443bdff467f0f52f895a
Inwoo Hwang <hsjkdss228@naver.com>
2022-07-16 02:42:15 +0900

    TransactionsPanel의 잔액 출력이 거래 이전의 원래 잔액을 출력하도록 수정

--- week04/MakaoBank/20220715-황인우/app/src/main/java/panels/TransactionsPanel.java
+++ week04/MakaoBank/20220715-황인우/app/src/main/java/panels/TransactionsPanel.java
@@ -4,25 +4,59 @@ import models.Account;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TransactionsPanel extends JPanel {
   public TransactionsPanel(Account myAccount, Account otherAccount) {
     this.setLayout(new GridLayout(0, 1));
 
     //내 계좌 거래 내역
-    this.add(new JLabel("계좌 번호: " + myAccount.identifier()));
-    this.add(new JLabel("잔액: " + myAccount.amount()));
+    JLabel myIdentifierLabel = new JLabel("계좌 번호: " + myAccount.identifier());
+    List<JLabel> myTransactionLabels = new ArrayList<>();
+
+    long myOriginalAmount = myAccount.amount();
 
     for (String transaction : myAccount.transactions()) {
-      this.add(new JLabel(transaction));
+      myTransactionLabels.add(new JLabel(transaction));
+
+      long transferAmount = Long.parseLong(transaction.replaceAll("\\D", ""));
+      transferAmount *= transaction.contains("출금") ? 1 : -1;
+
+      myOriginalAmount += transferAmount;
+    }
+
+    JLabel myOriginalAmountLabel = new JLabel("잔액: " + myOriginalAmount);
+
+    this.add(myIdentifierLabel);
+    this.add(myOriginalAmountLabel);
+
+    for (JLabel transactionLabel : myTransactionLabels) {
+      this.add(transactionLabel);
     }
 
     //상대 계좌 거래 내역
-    this.add(new JLabel("계좌 번호: " + otherAccount.identifier()));
-    this.add(new JLabel("잔액: " + otherAccount.amount()));
+    JLabel otherIdentifierLabel = new JLabel("계좌 번호: " + otherAccount.identifier());
+    List<JLabel> otherTransactionLabels = new ArrayList<>();
+
+    long otherOriginalAmount = otherAccount.amount();
 
     for (String transaction : otherAccount.transactions()) {
-      this.add(new JLabel(transaction));
+      otherTransactionLabels.add(new JLabel(transaction));
+
+      long transferAmount = Long.parseLong(transaction.replaceAll("\\D", ""));
+      transferAmount *= transaction.contains("출금") ? 1 : -1;
+
+      otherOriginalAmount += transferAmount;
+    }
+
+    JLabel otherOriginalAmountLabel = new JLabel("잔액: " + otherOriginalAmount);
+
+    this.add(otherIdentifierLabel);
+    this.add(otherOriginalAmountLabel);
+
+    for (JLabel transactionLabel : otherTransactionLabels) {
+      this.add(transactionLabel);
     }
   }
 }
