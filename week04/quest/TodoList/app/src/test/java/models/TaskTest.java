401aa02d1050b4b8ab6a3d3a3af761d734e8ceea
sunghwan22 <tidls3144@naver.com>
2022-07-16 21:28:37 +0900

    프로젝트 폴더 삭제

--- week04/quest/TodoList/app/src/test/java/models/TaskTest.java
+++ week04/quest/TodoList/app/src/test/java/models/TaskTest.java
@@ -7,34 +7,38 @@ import static org.junit.jupiter.api.Assertions.*;
 class TaskTest {
   @Test
   void creation() {
-    Task task = new Task("골든벨 공부하기",Task.PROGRESS);
+    Task task = new Task("골든벨 공부하기", Task.PROGRESS);
 
-    assertEquals("골든벨 공부하기" , task.task() );
+    assertEquals("골든벨 공부하기", task.task());
   }
+
   @Test
   void completeStatus() {
     Task task = new Task("기합넣기", Task.PROGRESS);
     task.setComplete();
 
-    assertEquals("COMPLETE" , task.state() );
+    assertEquals("COMPLETE", task.state());
   }
+
   @Test
   void deleteState() {
-    Task task = new Task("기합넣기",Task.PROGRESS);
+    Task task = new Task("기합넣기", Task.PROGRESS);
     task.setDelete();
 
-    assertEquals("DELETE" , task.state() );
+    assertEquals("DELETE", task.state());
   }
+
   @Test
   void progressState() {
-    Task task = new Task("기합넣기",Task.PROGRESS);
+    Task task = new Task("기합넣기", Task.PROGRESS);
     task.setProgress();
 
-    assertEquals("PROGRESS" , task.state() );
+    assertEquals("PROGRESS", task.state());
   }
+
   @Test
   void toCsvRow() {
-    Task task = new Task("으헤!",Task.PROGRESS);
+    Task task = new Task("으헤!", Task.PROGRESS);
     task.setComplete();
 
     assertEquals("으헤!,COMPLETE", task.toCsvRow());
