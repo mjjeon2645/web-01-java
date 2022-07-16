799aa1f2623787e4e866ad6a61038b002e2fcb2c
Inwoo Hwang <hsjkdss228@naver.com>
2022-07-16 15:33:47 +0900

    테스트 코드 일부 수정
    - 테스트에 입력되는 테스트 간략화
    - 테스트 메서드명, 테스트 코드에서 호출하는 객체의 메서드명 수정

--- week04/ToDoList/황인우/app/src/test/java/models/TaskTest.java
+++ week04/ToDoList/황인우/app/src/test/java/models/TaskTest.java
@@ -7,38 +7,39 @@ import static org.junit.jupiter.api.Assertions.*;
 class TaskTest {
   @Test
   void creation() {
-    Task task = new Task("레이시티 같은 오픈 월드 레이싱 게임의" +
-        " 게임플레이 월드를 구성하는 요소들에 뭐가 있을지 파악하기");
+    Task task = new Task("새로운 할 일 1");
+
+    assertNotNull(task);
   }
 
   @Test
   void text() {
-    Task task = new Task("포켓몬스터 게임 디렉터 되기");
+    Task task = new Task("새로운 할 일 2");
 
-    assertEquals("포켓몬스터 게임 디렉터 되기", task.text());
+    assertEquals("새로운 할 일 2", task.text());
   }
 
   @Test
-  void setStatusDone() {
-    Task task = new Task("자바 척척석사 되기");
-    task.setStatusDone();
+  void done() {
+    Task task = new Task("새로운 할 일 3");
+    task.done();
 
     assertEquals("DONE", task.status());
   }
 
   @Test
-  void setStatusToDo() {
-    Task task = new Task("토익 800점 넘겨서 졸업요건 맞추기");
-    task.setStatusDone();
-    task.setStatusToDo();
+  void processing() {
+    Task task = new Task("새로운 할 일 4");
+    task.done();
+    task.processing();
 
-    assertEquals("TO_DO", task.status());
+    assertEquals("PROCESSING", task.status());
   }
 
   @Test
-  void setStatusDeleted() {
-    Task task = new Task("반나절 쉬면서 심신 다잡기");
-    task.setStatusDeleted();
+  void delete() {
+    Task task = new Task("새로운 할 일 5");
+    task.delete();
 
     assertEquals("DELETED", task.status());
   }
