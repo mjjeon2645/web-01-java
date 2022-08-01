a6ce3691f4b5eb049c2751641b5b6a0b57c906e3
Inwoo Hwang <hsjkdss228@naver.com>
2022-08-02 04:18:16 +0900

    강의반복과제 마카오 뱅크 - 거래 내역 시작 지점 commit

---
+++ week07/makaoBankTransactions/20220801-황인우/app/src/test/java/utils/PageGeneratorTest.java
@@ -0,0 +1,29 @@
+package utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PageGeneratorTest {
+  @Test
+  void navigation() {
+    PageGenerator pageGenerator = new PageGenerator() {
+      @Override
+      public String content() {
+        return "";
+      }
+    };
+
+    String navigation = pageGenerator.navigation();
+
+    assertTrue(
+        navigation.contains("홈으로"), "navigation에 홈으로 링크 없음\n" + navigation
+    );
+    assertTrue(
+        navigation.contains("잔액 조회"), "navigation에 잔액 조회 링크 없음\n" + navigation
+    );
+    assertTrue(
+        navigation.contains("송금"), "navigation에 송금 링크 없음\n" + navigation
+    );
+  }
+}
