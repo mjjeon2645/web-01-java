182c4f99d70690fd8eb73afbc6e1b2e9d7a3b2b9
Jeon Minji <mjjeon2645@gmail.com>
2022-08-01 19:44:10 +0900

    20220801-makaoBank-transfer-전민지
    마카오뱅크 송금 강의 코드학습

---
+++ week07/makaoBankTransfer/20220801-전민지/app/src/main/java/MakaoBank.java
@@ -0,0 +1,100 @@
+import com.sun.net.httpserver.HttpServer;
+import models.Account;
+import repositories.AccountRepository;
+import services.TransferService;
+import utils.*;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.util.Map;
+
+public class MakaoBank {
+  private final FormParser formParser;
+
+  private final String accountIdentifier = "1234";
+
+  private final AccountRepository accountRepository;
+  private final TransferService transferService;
+
+  public static void main(String[] args) throws IOException {
+    MakaoBank makaoBank = new MakaoBank();
+    makaoBank.run();
+  }
+
+  public MakaoBank() {
+    formParser = new FormParser();
+
+    accountRepository = new AccountRepository();
+    transferService = new TransferService(accountRepository);
+  }
+
+  public void run() throws IOException {
+    InetSocketAddress address = new InetSocketAddress(8000);
+    HttpServer httpServer = HttpServer.create(address, 0);
+
+    httpServer.createContext("/", (exchange) -> {
+
+      // 1. 입력
+      URI requestURI = exchange.getRequestURI();
+      String path = requestURI.getPath();
+
+      String method = exchange.getRequestMethod();
+
+      String requestBody = new RequestBodyReader(exchange).body();
+
+      Map<String, String> formData = formParser.parse(requestBody);
+
+      // 2. 처리
+      PageGenerator pageGenerator = process(path, method, formData);
+
+      // 3. 출력
+      new MessageWriter(exchange).write(pageGenerator.html());
+    });
+
+    httpServer.start();
+
+    System.out.println("http://localhost:8000/");
+  }
+
+  public PageGenerator process(String path, String method,
+                               Map<String, String> formData) {
+
+    String[] steps = path.substring(1).split("/");
+    return switch (steps[0]) {
+      case "account" -> processAccount(steps.length > 1 ? steps[1] : "");
+      case "transfer" -> processTransfer(method, formData);
+      default -> new GreetingPageGenerator();
+    };
+  }
+
+  public AccountPageGenerator processAccount(String identifier) {
+    Account account = accountRepository.find(identifier, accountIdentifier);
+    return new AccountPageGenerator(account);
+  }
+
+  public PageGenerator processTransfer(String method,
+                                       Map<String, String> formData) {
+    if (method.equals("GET")) {
+      return processTransferGet();
+    }
+
+    return TransferProcessPost(formData);
+  }
+
+  public TransferPageGenerator processTransferGet() {
+    Account account = accountRepository.find(accountIdentifier);
+    return new TransferPageGenerator(account);
+  }
+
+  public TransferSuccessPageGenerator TransferProcessPost(
+      Map<String, String> formData) {
+    transferService.transfer(
+        accountIdentifier,
+        formData.get("to"),
+        Long.parseLong(formData.get("amount")));
+
+    Account account = accountRepository.find(accountIdentifier);
+    return new TransferSuccessPageGenerator(account);
+  }
+}
