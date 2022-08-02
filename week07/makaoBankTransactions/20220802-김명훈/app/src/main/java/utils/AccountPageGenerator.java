ddc606c8eb903b51f3c04462092c20a9bfe4c18b
myunghoonkim <xcv8826@gmail.com>
2022-08-02 21:25:20 +0900

    20220802-makaoBankTransaction-김명훈
    주간반복과제

---
+++ week07/makaoBankTransactions/20220802-김명훈/app/src/main/java/utils/AccountPageGenerator.java
@@ -0,0 +1,21 @@
+package utils;
+
+import models.Account;
+
+public class AccountPageGenerator extends PageGenerator {
+
+  private Account account;
+
+  public AccountPageGenerator(Account account) {
+    super();
+
+    this.account = account;
+  }
+
+  @Override
+  public String content() {
+    return "<p>계좌 번호: " + account.identifier() + "</p>" +
+        "<p>이름: " + account.name() + "</p>" +
+        "<p>잔액: " + account.amount() + "</p>";
+  }
+}
---
+++ week07/makaoBankTransactions/20220802-김명훈/app/src/main/java/utils/TransferSuccessPageGenerator.java
@@ -0,0 +1,17 @@
+package utils;
+
+import models.Account;
+
+public class TransferSuccessPageGenerator extends PageGenerator {
+  private Account account;
+
+  public TransferSuccessPageGenerator(Account account) {
+    super();
+    this.account = account;
+  }
+
+  @Override
+  public String content() {
+    return "계좌 이체 성공";
+  }
+}
