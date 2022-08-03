7c1e699405ea2e3b6eced13470c13f477e3a65b3
Jeon Minji <mjjeon2645@gmail.com>
2022-08-03 14:21:59 +0900

    20220803-짝프로그래밍-조성환-전민지
    20220803-makaoBankTransfer

---
+++ week07/makaoBankTransfer/20220803-조성환-전민지/app/src/test/java/utils/GreetingPageGeneratorTest.java
@@ -0,0 +1,43 @@
+package utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class GreetingPageGeneratorTest {
+  @Test
+  void text() {
+    PageGenerator pageGenerator = new GreetingPageGenerator();
+
+    String html = pageGenerator.html();
+
+    assertTrue(html.contains("Hello, world!"));
+  }
+
+  @Test
+  void textWithName() {
+    PageGenerator pageGenerator = new GreetingPageGenerator("Ashal");
+
+    String html = pageGenerator.html();
+
+    assertTrue(html.contains("Hello, Ashal!"));
+  }
+
+  @Test
+  void textWithEmpty() {
+    PageGenerator pageGenerator = new GreetingPageGenerator("");
+
+    String html = pageGenerator.html();
+
+    assertTrue(html.contains("Hello, world!"));
+  }
+
+  @Test
+  void textWithBlank() {
+    PageGenerator pageGenerator = new GreetingPageGenerator("    ");
+
+    String html = pageGenerator.html();
+
+    assertTrue(html.contains("Hello, world!"));
+  }
+}
