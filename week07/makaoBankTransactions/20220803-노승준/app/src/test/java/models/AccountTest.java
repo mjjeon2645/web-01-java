4d2edd80bb8a1fecb7a6f0e4fd09b3d0976de90c
seungjjun <il1uso@naver.com>
2022-08-03 16:14:10 +0900

    7주차 수요일 주간과제 마카오뱅크-거래 내역

---
+++ week07/makaoBankTransactions/20220803-노승준/app/src/test/java/models/AccountTest.java
@@ -0,0 +1,56 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+  @Test
+  void creation() {
+    Account account = new Account("1234", "ashal", 3000);
+
+    assertEquals("1234", account.identifier());
+    assertEquals("ashal", account.name());
+    assertEquals(3000, account.amount());
+  }
+
+  @Test
+  void transfer() {
+    Account sender = new Account("1234", "ashal", 3000);
+    Account receiver = new Account("2345", "joker", 500);
+
+    int oldSenderTransactionsCount = sender.transactionsCount();
+    int oldReceiverTransactionsCount = receiver.transactionsCount();
+
+    sender.transfer(receiver, 1000);
+
+    assertEquals(2000, sender.amount());
+    assertEquals(1500, receiver.amount());
+
+    int newSenderTransactionsCount = sender.transactionsCount();
+    int newReceiverTransactionsCount = receiver.transactionsCount();
+
+    assertEquals(1, newSenderTransactionsCount - oldSenderTransactionsCount);
+    assertEquals(1, newReceiverTransactionsCount - oldReceiverTransactionsCount);
+  }
+
+  @Test
+  void transactions() {
+    Account sender = new Account("1234", "ashal", 3000);
+    Account receiver = new Account("2345", "joker", 500);
+
+    sender.transfer(receiver, 1000);
+
+    assertEquals(1, sender.transactions().size());
+    assertEquals(1, receiver.transactions().size());
+  }
+
+  @Test
+  void equals() {
+    Account account1 = new Account("1234", "ashal", 3000);
+    Account account2 = new Account("2345", "joker", 500);
+
+    assertTrue(account1.equals(account1));
+    assertFalse(account1.equals(account2));
+  }
+}
