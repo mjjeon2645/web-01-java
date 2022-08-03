f8affa5e329b24c78ff730fcbf7580aacb216b82
Jeon Minji <mjjeon2645@gmail.com>
2022-08-03 23:15:49 +0900

    20220804-마카오뱅크 잔액조회-전민지
    주간 강의 반복과제20220804-마카오뱅크 잔액조회-전민지
    
    주간 강의 반복과제

---
+++ week07/MakaoBank-잔액조회/20220804-전민지/app/src/main/java/utils/GreetingPageGenerator.java
@@ -0,0 +1,21 @@
+package utils;
+
+public class GreetingPageGenerator extends PageGenerator {
+  private String name;
+
+  public GreetingPageGenerator() {
+    this.name = "world";
+  }
+
+  public GreetingPageGenerator(String name) {
+    if (name.isBlank()) {
+      this.name = "world";
+      return;
+    }
+    this.name = name;
+  }
+
+  public String html() {
+    return "Hello, " + name + "!";
+  }
+}
