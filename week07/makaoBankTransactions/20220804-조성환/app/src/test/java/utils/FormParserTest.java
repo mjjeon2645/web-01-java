43ca9746b8d052c86c4050b658b960735c48707e
sunghwan22 <tidls3144@naver.com>
2022-08-04 17:07:37 +0900

    마카오뱅크 거래내역

---
+++ week07/makaoBankTransactions/20220804-조성환/app/src/test/java/utils/FormParserTest.java
@@ -0,0 +1,29 @@
+package utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.Normalizer;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FormParserTest {
+  @Test
+  void parse() {
+    FormParser formParser = new FormParser();
+
+    Map<String, String> formData = formParser.parse("to=2345&amount=1000");
+
+    assertEquals("2345", formData.get("to"));
+    assertEquals("1000", formData.get("amount"));
+  }
+
+  @Test
+  void parseWithBlank() {
+    FormParser formParser = new FormParser();
+
+    Map<String, String> formData = formParser.parse("");
+
+    assertFalse(formData.containsKey("to"));
+  }
+}
