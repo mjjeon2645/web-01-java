4dd9d58fddacf7669c1b7262aa16086b6ea05820
myunghoonkim <xcv8826@gmail.com>
2022-08-04 18:28:09 +0900

    20220804-주간강의반복과제-송금-김명훈
    송금

---
+++ week07/makaoBankTransfer/20220804-김명훈/app/src/main/java/models/Account.java
@@ -0,0 +1,34 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Account {
+  private String identifier;
+  private String name;
+  private long amount;
+
+  public Account(String identifier, String name, long amount) {
+
+    this.identifier = identifier;
+    this.name = name;
+    this.amount = amount;
+  }
+
+  public String identifier() {
+    return identifier;
+  }
+
+  public String name() {
+    return name;
+  }
+
+  public long amount() {
+    return amount;
+  }
+
+  public void transfer(Account receiver, long amount) {
+    this.amount -= amount;
+    receiver.amount += amount;
+  }
+}
